ce8aa173c803e1f09a75821e396d1af60720db27
Shaun Thornton <homeworkhopper@gmail.com>
2023-05-07 15:18:12 -0400

    Thistlethwaite stage move generation

--- src/com/homeworkhopper/decube/cube/ThistlethwaiteStage.java
+++ src/com/homeworkhopper/decube/cube/ThistlethwaiteStage.java
@@ -1,7 +1,5 @@
 package com.homeworkhopper.decube.cube;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,7 +7,19 @@ import java.util.function.Predicate;
 public enum ThistlethwaiteStage implements Predicate<CubeConfiguration>, Function<CubeConfiguration, Iterable<CubeConfiguration>> {
 
     // G0=<L,R,F,B,U,D>
-    G0(FaceTurn.LEFT_TURN, FaceTurn.RIGHT_TURN, FaceTurn.FRONT_TURN, FaceTurn.BACK_TURN, FaceTurn.UP_TURN, FaceTurn.DOWN_TURN) {
+    G0 {
+        @Override
+        public Iterable<CubeConfiguration> apply(CubeConfiguration configuration) {
+            return Set.of(
+                    FaceTurn.LEFT_TURN.apply(configuration),
+                    FaceTurn.RIGHT_TURN.apply(configuration),
+                    FaceTurn.FRONT_TURN.apply(configuration),
+                    FaceTurn.BACK_TURN.apply(configuration),
+                    FaceTurn.UP_TURN.apply(configuration),
+                    FaceTurn.DOWN_TURN.apply(configuration)
+            );
+        }
+
         @Override
         public boolean test(CubeConfiguration configuration) {
             return false;
@@ -17,7 +27,19 @@ public enum ThistlethwaiteStage implements Predicate<CubeConfiguration>, Functio
     },
 
     // G1=<L,R,F,B,U2,D2>
-    G1() {
+    G1 {
+        @Override
+        public Iterable<CubeConfiguration> apply(CubeConfiguration configuration) {
+            return Set.of(
+                    FaceTurn.LEFT_TURN.apply(configuration),
+                    FaceTurn.RIGHT_TURN.apply(configuration),
+                    FaceTurn.FRONT_TURN.apply(configuration),
+                    FaceTurn.BACK_TURN.apply(configuration),
+                    FaceTurn.UP_TURN.andThen(FaceTurn.UP_TURN).apply(configuration),
+                    FaceTurn.DOWN_TURN.andThen(FaceTurn.DOWN_TURN).apply(configuration)
+            );
+        }
+
         @Override
         public boolean test(CubeConfiguration configuration) {
             return false;
@@ -27,6 +49,18 @@ public enum ThistlethwaiteStage implements Predicate<CubeConfiguration>, Functio
     // G2=<L,R,F2,B2,U2,D2>
     G2 {
         @Override
+        public Iterable<CubeConfiguration> apply(CubeConfiguration configuration) {
+            return Set.of(
+                    FaceTurn.LEFT_TURN.apply(configuration),
+                    FaceTurn.RIGHT_TURN.apply(configuration),
+                    FaceTurn.FRONT_TURN.andThen(FaceTurn.FRONT_TURN).apply(configuration),
+                    FaceTurn.BACK_TURN.andThen(FaceTurn.BACK_TURN).apply(configuration),
+                    FaceTurn.UP_TURN.andThen(FaceTurn.UP_TURN).apply(configuration),
+                    FaceTurn.DOWN_TURN.andThen(FaceTurn.DOWN_TURN).apply(configuration)
+            );
+        }
+
+        @Override
         public boolean test(CubeConfiguration configuration) {
             return false;
         }
@@ -34,6 +68,18 @@ public enum ThistlethwaiteStage implements Predicate<CubeConfiguration>, Functio
 
     // G3=<L2,R2,F2,B2,U2,D2>
     G3 {
+        @Override
+        public Iterable<CubeConfiguration> apply(CubeConfiguration configuration) {
+            return Set.of(
+                    FaceTurn.LEFT_TURN.andThen(FaceTurn.LEFT_TURN).apply(configuration),
+                    FaceTurn.RIGHT_TURN.andThen(FaceTurn.RIGHT_TURN).apply(configuration),
+                    FaceTurn.FRONT_TURN.andThen(FaceTurn.FRONT_TURN).apply(configuration),
+                    FaceTurn.BACK_TURN.andThen(FaceTurn.BACK_TURN).apply(configuration),
+                    FaceTurn.UP_TURN.andThen(FaceTurn.UP_TURN).apply(configuration),
+                    FaceTurn.DOWN_TURN.andThen(FaceTurn.DOWN_TURN).apply(configuration)
+            );
+        }
+
         @Override
         public boolean test(CubeConfiguration configuration) {
             return false;
@@ -43,25 +89,13 @@ public enum ThistlethwaiteStage implements Predicate<CubeConfiguration>, Functio
     // G4=I (solved)
     G4 {
         @Override
+        public Iterable<CubeConfiguration> apply(CubeConfiguration configuration) {
+            return Set.of(configuration);   // a solved cube should not be succeeded
+        }
+
+        @Override
         public boolean test(CubeConfiguration configuration) {
             return false;
         }
     };
-
-    private final Set<TurnMove> moves;
-
-    ThistlethwaiteStage(TurnMove... moves) {
-        this.moves = Set.of(moves);
-    }
-
-    @Override
-    public Iterable<CubeConfiguration> apply(CubeConfiguration configuration) {
-        final List<CubeConfiguration> configurations = new ArrayList<>(moves.size());
-
-        for (TurnMove move : moves) {
-            configurations.add(move.apply(configuration));
-        }
-
-        return configurations;
-    }
 }
